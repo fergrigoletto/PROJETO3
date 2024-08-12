d30e9e6e1d3e6a288701842391eb4410278fd37b
fergrigoletto <00001092452746sp@al.educacao.sp.gov.br>
2024-08-12 17:26:00 -0300

    Update and rename README.md to sketch.js

---
+++ sketch.js
@@ -0,0 +1,30 @@
+# PROJETO DE TRES BOLAS QUE ATRAVESSAM A TELA E PISCAM 🙌✨
+let cor;
+let circuloY;
+let circuloX;
+
+function setup() {
+  createCanvas(1000, 800);
+   background("white");
+  cor=color(random(0,225),random(0,225),random(0,225));
+  circuloY = [random(height),random(height),random(height)];
+  circuloX = [0,0,0];
+}
+
+function draw() {
+  fill(cor);
+for (let quantos in circuloX)  {
+  circle(circuloX[quantos],circuloY[quantos],100);
+  circuloX[quantos]+= random(0,3);
+  circuloY[quantos]+= random(-3,3);
+  
+  if(circuloX[quantos] >= width) {
+    circuloX[quantos] = 0;
+    circuloY[quantos] = random(height); 
+  }
+}
+  
+  if (mouseIsPressed ){
+  cor=color(random(0,255),random(0,255),random(0,255),random(0,100));
+  }
+}  
